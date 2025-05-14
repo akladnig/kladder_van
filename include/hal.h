b46c84ab40d75ce17a7e845dcc54ac7d1e065d22
akladnig <adrian@kladnig.com>
2025-05-14 19:48:35 +1000

    First commit of header file to store ESP32 pin allocations

---
+++ include/hal.h
@@ -0,0 +1,12 @@
+
+#ifndef _HAL_H
+#define _HAL_H
+
+// Data wire is plugged into pin 15 on the ESP32
+#define ONE_WIRE_BUS 15
+
+// OLED Display pins - SCL and SDA are already defined by ESP32
+#define SCL_OLED 22
+#define SDA_OLED 21
+
+#endif
