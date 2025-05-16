8bd79acd83a2104e0a58520d9ec800cdf5f47110
akladnig <adrian@kladnig.com>
2025-05-16 21:15:20 +1000

    added define to select home network or van

--- include/configuration.h
+++ include/configuration.h
@@ -1,10 +1,22 @@
 #ifndef _CONFIGURATION_H
 #define _CONFIGURATION_H
 
+#define USE_HOME_WIFI
+
 #define HOME_SSID "Curie"
 #define HOME_PWD "two2dogs"
 
 #define VAN_SSID "kladderVan"
 #define VAN_PWD "two2dogs"
+
+#ifdef USE_HOME_WIFI
+#define WIFI_MODE WIFI_MODE_STA
+#define MY_SSID HOME_SSID
+#define MY_PWD HOME_PWD
+#else
+#define WIFI_MODE WIFI_MODE_AP
+#define MY_SSID VAN_SSID
+#define MY_PWD VAN_PWD
+#endif
 
 #endif
