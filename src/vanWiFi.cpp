4756cad85f098426a2bcd2e6f30885c1a48b61da
akladnig <adrian@kladnig.com>
2025-05-16 21:17:34 +1000

    added fixed IP address

--- src/vanWiFi.cpp
+++ src/vanWiFi.cpp
@@ -9,11 +9,21 @@ WiFiServer wifiServer(80);
 
 void initWiFi()
 {
-  // Network credentials for the van wifi network.
+  // Network credentials for the home wifi network.
   const char *ssid = HOME_SSID;
   const char *password = HOME_PWD;
 
+  IPAddress local_IP(192, 168, 20, 7);
+  IPAddress gateway(192, 168, 20, 1);
+  IPAddress subnet(255, 255, 255, 0);
+
   WiFi.mode(WIFI_STA);
+
+  if (!WiFi.config(local_IP, gateway, subnet))
+  {
+    Serial.println("STA Failed to configure");
+  }
+
   WiFi.begin(ssid, password);
   Serial.printf("Trying to connect [%s] ", WiFi.macAddress().c_str());
   while (WiFi.status() != WL_CONNECTED)
